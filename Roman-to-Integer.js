bb816626a1f3f80636aa8ffe76f96844df748956
Pinedaberg <142705445+Pinedaberg@users.noreply.github.com>
2023-11-17 02:21:29 -0500

    Update Roman-to-Integer.js

--- Roman-to-Integer.js
+++ Roman-to-Integer.js
@@ -45,22 +45,19 @@
 
 //exersice 
 
-class Solution {
-public:
-    int romanToInt(string s) {
-        int ans=0;
-        unordered_map <char,int> mp{
-        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-
-    for(int i=0;i<s.size();i++){
-        if(mp[s[i]]<mp[s[i+1]]){
-            //for cases such as IV,CM, XL, etc...
-            ans=ans-mp[s[i]];
-        }
-        else{
-            ans=ans+mp[s[i]];
-        }
-    }
-        return ans;
-    }
+symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
 };
+
+var romanToInt = function(s) {
+    value = 0;
+    for(let i = 0; i < s.length; i+=1){
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    }
+    return value
